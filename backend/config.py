08365563812d45190c8bd4e4d2813e799a577d85
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-29 11:54:41 -0400

    Removed flask_mail code from config.py

--- backend/config.py
+++ backend/config.py
@@ -14,10 +14,4 @@ class Config(object):
 
     SQLALCHEMY_DATABASE_URI = _raw_database_url or ('sqlite:///' + os.path.join(basedir, 'app.db'))
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['cs205testingfall23@gmail.com']
 
